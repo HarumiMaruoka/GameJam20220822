89a7726518d510c923b1b45ab87ccbc4b4ea5ee2
RyutoOhmori <ryutoomori0414@gmail.com>
2022-08-22 11:41:04 +0900

    [add] 基本的なenemyの動きの制作

---
+++ Assets/Ohmori/Enemy1Controller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Enemy1Controller : MonoBehaviour
+{
+    Transform _targetTransform;
+    /// <summary>移動速度</summary>
+    [Tooltip("移動速度")]
+    [SerializeField] float _moveSpeed = default;
+    /// <summary>ターゲットのタグ名</summary>
+    [Tooltip("ターゲットのタグ名")]
+    [SerializeField] string _targetTag = default;
+    Rigidbody2D _rb;
+    void Start()
+    {
+        _targetTransform = GameObject.FindWithTag(_targetTag).transform;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _rb.velocity = (_targetTransform.position - transform.position).normalized * _moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
